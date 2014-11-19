e8cd6c8910b3dc7f51b1131d6467ad49d45b0119
Dariusz Kobylarz <darek.kobylarz@gmail.com>
2014-11-20 00:56:42 +0100

    initial version of vertx app

---
+++ aspen-datawarehouse/src/main/groovy/ai/vital/aspen/datawarehouse/main/AspenDatawarehouseMain.groovy
@@ -0,0 +1,60 @@
+package ai.vital.aspen.datawarehouse.main
+
+import ai.vital.aspen.datawarehouse.rest.WebServer;
+
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigFactory
+
+import org.vertx.java.core.AsyncResult
+import org.vertx.java.core.AsyncResultHandler
+import org.vertx.java.core.json.JsonObject
+import org.vertx.java.platform.PlatformLocator
+import org.vertx.java.platform.PlatformManager
+
+
+/**
+ * Main aspen datawarehouse vert.x app starter
+ * @author Derek
+ *
+ */
+class AspenDatawarehouseMain {
+
+	static main(args) {
+	
+		if(args.length != 1) {
+			System.err.println("No config file!")
+			System.exit(1)
+		}
+	
+		PlatformManager platformManager = PlatformLocator.factory.createPlatformManager();
+		
+		File configFile = new File(args[0])
+		
+		println "Config file: ${configFile.absolutePath}"
+		
+		Config config = ConfigFactory.parseFile(configFile)
+		
+		Config webServerCfg = config.getConfig("webserver")
+	
+		platformManager.deployVerticle(WebServer.class.getCanonicalName(), new JsonObject(webServerCfg.root().unwrapped()), new URL[0], 1, null, new AsyncResultHandler<String>() {
+			public void handle(AsyncResult<String> asyncResult) {
+				if (asyncResult.succeeded()) {
+					println(WebServer.class.getCanonicalName() + " deployment ID is " + asyncResult.result());
+				} else {
+					System.err.println "Couldn't deploy webserver verticle: " + asyncResult.cause().localizedMessage;
+					System.exit(1)
+				}
+			}
+		})
+		
+		//prevent the app from exiting
+		
+		while(true) {
+			
+			Thread.sleep(60000)
+			
+		}
+		
+	}
+
+}
---
+++ aspen-datawarehouse/web/js/pages/jobs.js
@@ -0,0 +1,60 @@
+$(function(){
+
+	$(document).bind(EVENTBUS_CONNECT, function(){
+
+		eventbus_jobs_list(onJobsListReady, onJobsListError);
+	
+	});
+
+});
+
+var jobRowTemplate = null
+
+function onJobsListReady(result) {
+
+  if( jobRowTemplate == null ) {
+  
+    jobRowTemplate = $('#job-template').remove().removeAttr('id').css('display', '');
+    
+  }
+  
+  handleJobsList($('#running-jobs'), result.running);
+  
+  handleJobsList($('#complete-jobs'), result.complete);
+  
+  handleJobsList($('#failed-jobs'), result.failed);
+  
+  
+  
+  
+  
+
+}
+
+function onJobsListError(message) {
+  alert(message);
+}
+
+function handleJobsList(tableEl, jobs) {
+
+  tableEl.find('tr:not(:first)').remove();
+  
+  for(var i = 1 ; i <= jobs.length; i++ ) {
+    
+    var job = jobs[i-1];
+    
+    var t = jobRowTemplate.clone();
+    
+    t.find('.index').text( i + '.');
+    t.find('.job-id').text( job.id );
+    t.find('.classpath').text(job.cp);
+    t.find('.context').text(job.context);
+    t.find('.start-time').text(job.startTime);
+    t.find('.duration').text(job.duration);
+    
+    tableEl.append(t);
+    
+  }
+
+
+}
