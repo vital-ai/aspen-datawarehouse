c05d325e7cca2761baa8da859c13a6fce725146a
hadfield <marc@vital.ai>
2014-11-30 13:28:53 -0500

    planning vitalservice integration

---
+++ aspen-datawarehouse/src/main/groovy/ai/vital/aspen/datawarehouse/service/SparkVitalServiceREST.groovy
@@ -0,0 +1,14 @@
+package ai.vital.aspen.datawarehouse.service
+
+class SparkVitalServiceREST {
+
+	// REST handler, similar to vital prime implementation
+	
+	// client implementation in vitalservice-source
+	
+	// calls SparkVitalServiceImpl in scala codebase
+	
+	
+	
+	
+}
