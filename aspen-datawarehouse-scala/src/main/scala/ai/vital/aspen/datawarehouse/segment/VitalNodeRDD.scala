8a8d3b23e332336515ea82727d445d395cef163e
hadfield <marc@vital.ai>
2014-12-02 13:07:55 -0500

    tweaks

---
+++ aspen-datawarehouse-scala/src/main/scala/ai/vital/aspen/datawarehouse/segment/VitalNodeRDD.scala
@@ -0,0 +1,11 @@
+package ai.vital.aspen.datawarehouse.segment
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd._
+import ai.vital.aspen.model._
+import org.apache.spark._
+
+
+class VitalNodeRDD {
+  
+}
